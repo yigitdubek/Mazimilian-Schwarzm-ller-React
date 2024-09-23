2d86a3b686cb09dd2d35b342c151f299c32e7dea
yigitdubek <56270048+yigitdubek@users.noreply.github.com>
2024-09-23 13:26:43 +0300

    Delete src directory

--- src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
